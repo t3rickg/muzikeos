69ce16042fb3320a16a60843c07e827dda664ec5
koreandrama12300 <128028635+koreandrama12300@users.noreply.github.com>
2023-03-18 22:35:36 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -7,7 +7,7 @@ class Config(object):
     STRING_SESSION = os.environ.get("STRING_SESSION", BQAmCkokq9hFVY1cIDltCdPn_35HGg11zmXkaaZe5K8jxHI0Mr7wEG8_7Yj4cjlHO6vPRNFdQQ65R3txN2o3ghIDG8d4XvxGLw-49CGD2XmzD7zvG4QNJHlXqfuOSdf4WR4FmS6VIyoymceSFVqt_nzbIaOq2PhjpcKEkqezxvfJ1Sl9zYvAq55RaAWXxmTysz_HL8S1GhfMQM-O0-3bedEFS_Bvr-WdYAGNlSsC5XOc8fHNq1DP_5tSTH-GQyik_LSMFhebYt9lLCWL2_sFBkmi8sIYml5IYfZiBx154siJcU57yRNMXOdwT61JcfyUhujdIE1mzAAN26aQ1i2uvcDhAAAAAVrwuVEA)
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "koreandrama1230bot")
     SUPPORT = os.environ.get("SUPPORT", "TheSupportChat") # Your Support
     CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel") # Your Channel
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
