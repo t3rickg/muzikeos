be9390e03112ba24a04b12f30c84059f7d769f69
8918899789 <129154771+8918899789@users.noreply.github.com>
2023-03-28 14:27:17 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", "6213538"))
-    API_HASH = os.environ.get("API_HASH", "8ce3522bd21cc937eee4c68813d501d5")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-    STRING_SESSION = os.environ.get("STRING_SESSION", None)
+    API_ID = int(os.environ.get("API_ID", "20482153"))
+    API_HASH = os.environ.get("API_HASH", "e55cfcf3751a6543603b292b9cbd041e")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6069064073:AAH6l2qMaiyJ1JKpVufHpv6TW9krSiGN2LE")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOHQBu1Gw5DYwcZKBc6s76j6Ch9HBXr_RFR-5pW3t_QZD7xidABal97LrZUpkEPprZFrEbd5tr-kaMqsKT10zZl_DxqA1O40egTtB-jO9KLpt7JDS0iyyJX-DebPfQSkHOMq9FYdVyCGa_YcUwgbkt7znbQWEqVgyjwk2A5hdwsMdDfoNHuuxu-140n1VEw3G0wJAORgCJ-cD5Ovy0faO0gsAuvmrYOD_zJCQZJG_dAs25LUpKevf9SnlTN-AFqEItk6NYQG05iMVnZg8eWPv-adqOLvjiIxDCV9I5xTQjLxFvSkgCtHwNjAKrFpTvrl9-bTLtd5vPnZ_y4gQSStqBsqwLGk=")
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "ayushkrh_music_bot")
     SUPPORT = os.environ.get("SUPPORT", "TheSupportChat") # Your Support
     CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel") # Your Channel
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
